78405c52472d54158445631fc06b73f260fc6548
DimSan <DiS-ne@ukr.net>
2015-04-28 02:05:50 +0300

    href iterator and replase

--- homeW8DS.php
+++ homeW8DS.php
@@ -1,24 +1,22 @@
 <?php
 require_once 'arrays.php';
 
-echo"<pre>";
-print_r($arr);
-echo"</pre>";
-echo'--------------------------------';
 reckOut($arr);
 
-function reckOut ($cehc) {
+function reckOut ($cehc,$i=1) {
     echo"<ul>";
     if (is_array($cehc)){
         foreach ($cehc as $k=>$v){
-            if(is_array($v)){                
-                echo "<li>"."<a href=".$k.">".$k."</a>";
-                reckOut($v);
+            if(is_array($v)){  
+                $j=$i;
+                echo "<li>"."<a href=\"#".str_replace(',', '.', $i)."\">".$k."</a>";
+                reckOut($v,$j.=',1');
                 echo "</li>";
                 
-            }else{echo "<li>"."<a href=".$k.">".$v."</a>"."</li>";}
+            }else{echo "<li>"."<a href=\"#".str_replace(',', '.', $i)."\">".$v."</a>"."</li>";}
+            $i++;
         }
-    }else{echo "<li>"."<a href=".$cehc.">".$cehc."</li>";}
+    }else{echo "<li>"."<a href=\"#".str_replace(',', '.', $i)."\">".$cehc."</li>";}
     echo "</ul>";
 }
 
@@ -26,6 +24,7 @@ function reckOut ($cehc) {
 
 
 ?>
+<h3>Model</h3>
 <ul>
     <li>
         <a href="#1">Раздел 1</a>
